7817300692bf27052387bc1a1f649cecf41eb8a3
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-12-13 14:49:18 +0200

    intial add cleanuptrait

---
+++ src/Models/EmailsTable/EmailsTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nip\Mail\Models\EmailsTable;
+
+use Nip\Mail\Models\Cleanup\RecordsTrait;
+
+/**
+ * Trait EmailsTrait
+ * @package Nip\Mail\Models\EmailsTable
+ */
+trait EmailsTrait
+{
+    use RecordsTrait;
+}
