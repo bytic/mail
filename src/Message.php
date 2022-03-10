d57cb1adc52cc32b2ebb3071e6e7f5ec58b0ac8e
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-03-11 00:01:57 +0200

    feat: fix message

--- src/Message.php
+++ src/Message.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Nip\Mail;
 
@@ -9,4 +10,13 @@ class Message extends \Symfony\Component\Mime\Email
 {
     use Message\HasMergeArgsTrait;
     use Message\HasAttachmentsTrait;
+
+    /**
+     * @param $from
+     * @return $this
+     */
+    public function setFrom($from): Message
+    {
+        return $this->addFrom($from);
+    }
 }
